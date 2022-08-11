8a519b314ab1fcad5a656791c8edfe4b1e4874fe
valentyn <valentyn.l@officelabs.me>
2022-08-11 10:21:14 +0200

    setup store, reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {useDispatch, useSelector} from "react-redux";
+import {RootState} from "./redux/store";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const { count } = useSelector((state: RootState) => state.team)
+    const dispatch = useDispatch()
+
+    return (
+        <div className="App">
+            <div>
+                <div>Hello React</div>
+                <div>{count}</div>
+                <button onClick={() => dispatch({type: 'INCREMENT'})}>increment</button>
+                <button onClick={() => dispatch({type: 'DECREMENT'})}>decrement</button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/redux/rootReducer.ts
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import teamReducer from './teamReducer';
+
+const rootReducer = combineReducers({
+    team: teamReducer,
+});
+
+export default rootReducer;
